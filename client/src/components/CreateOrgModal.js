00b3318bd8c73ff65306fc9561b5019d6b31d90e
sasikant <nairs999@gmail.com>
2019-10-15 21:53:50 +0530

    Add mapPropsToDispatch in createOrgModal

--- client/src/components/CreateOrgModal.js
+++ client/src/components/CreateOrgModal.js
@@ -6,19 +6,12 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import { createOrganisation } from '../actions/Action';
 
 const initialState = {
-	selectedFile: null,
 	orgName: '',
 	location: ''
 }
 
 class CreateOrgModal extends React.Component {
 	state = initialState;
-
-	onChangeHandler = (e) => {
-		this.setState({
-			selectedFile: event.target.files[0]
-		});
-	}
 
 	changeValue = (e) => {
 		const key = e.target.name;
@@ -27,40 +20,28 @@ class CreateOrgModal extends React.Component {
 	}
 
 	onClickHandler = (e) => {
-		const { selectedFile, orgName, location, creator } = this.state;
-		const { handleCreateOrgModal } = this.props;
+		const { orgName, location, creator } = this.state;
+		const { handleCreateOrgModal, createOrganisation } = this.props;
 		e.preventDefault();
 
-		// if(!this.state.selectedFile || !this.state.orgName || !this.state.location) {
-		//   return alert('File, Name and Location are must.');
-		// }
-
-		// if(!this.state.selectedFile.name.endsWith('.png') || !this.state.selectedFile.name.endsWith('.jpeg') || !this.state.selectedFile.name.endsWith('.jpg')) {
-		//   console.log(this.state.selectedFile, 'thisisselectedFile');
-		//   return alert("Image format must be .jpg, .jpeg, or .png.");
-		// }
-
-		if (this.state.selectedFile.size >= 10000000 / 2) {
-			return alert('Image file size must be below 5MB');
-		}
-
 		const data = new FormData();
-		data.append('file', selectedFile);
+		// data.append('file', selectedFile);
 		data.append('name', orgName);
 		data.append('location', location);
 		data.append('creator', creator);
 
-		this.props.dispatch(createOrganisation(data));
+		createOrganisation(data);
 		this.setState(initialState, () => handleCreateOrgModal());
 	}
 
 	handleCloseModal = () => {
+		const { handleCreateOrgModal } = this.props;
 		this.setState(initialState, () => handleCreateOrgModal());
 	}
 
 
 	render() {
-		const { handleCreateOrgModal, isCreateOrgModalOpen } = this.props;
+		const { isCreateOrgModalOpen } = this.props;
 
 		return (
 			<Dialog onClose={this.handleCloseModal} aria-labelledby="simple-dialog-title" open={isCreateOrgModalOpen}>
@@ -73,20 +54,6 @@ class CreateOrgModal extends React.Component {
 							</div>
 							<div className="fiv">
 								<input className='input' type="text" value={this.state.location} onChange={this.changeValue} name="location" placeholder='location' />
-							</div>
-							<div className="fiv">
-								<label htmlFor="upload-image">
-									<input
-										accept="image/*"
-										name="file"
-										type="file"
-										id="upload-image"
-										style={{ display: 'none' }}
-										onChange={this.onChangeHandler}
-									/>
-									<i className="fas fa-cloud-upload-alt"></i>
-									Select Image
-                </label>
 							</div>
 							<button type="submit" className="button bg-primary">Create</button>
 						</div>
@@ -103,4 +70,4 @@ const mapStateToProps = (state) => {
 	}
 }
 
-export default connect(mapStateToProps)(CreateOrgModal);
+export default connect(mapStateToProps, { createOrganisation })(CreateOrgModal);
